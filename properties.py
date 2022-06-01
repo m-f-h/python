03fb74e3b1c681647a31d133d49494c6d8390970
m-f-h <github@dsi972.fr>
2022-06-01 01:44:48 -0400

    Update properties.py
    Minor corrections in text (documentation & code).

--- properties.py
+++ properties.py
@@ -1,6 +1,5 @@
 """ Properties in Python.
   Written & (c) May 2022 by M.F.Hasler
-  "attributs - méthode" fonctionnent dans Python,
 
 A property is a kind of "computed attribute" of a Python object: 
 It can be accessed as if it was a simple attribute, but it is actually computed on demand when accessed
@@ -24,14 +23,20 @@ class temperature():
   def F(self): return self.K*9/5 - 459.67
   @F.setter
   def F(self, value): self.K = (value + 459.67)*5/9
+
   def __repr__(self):
+    "Return a string representation of this temperature instance."
     return type(self).__name__ + "(%g)" % self.K
+
   def __str__(self):
-    return "%g K = %g °C = %g °F" % ( T.K, T.C, T.F )
+    "Return a string for nicely displaying the temperature."
+    return "%g K = %g °C = %g °F" % (self.K, self.C, self.F)
+  
   def __init__(self, value = 0, C = None, F = None):
-    """Initialize the temperature with a string that may contain K, C or F,
-    or with a floating point value that can be given as C = ... or F = ...
-    if a scale different from Kelvin is desired."""
+    """Initialize the temperature with a string that may contain
+    any of 'C', 'F' or 'K' and whitespace and '°' signs which are ignored,
+    or with a floating point value that can be given as named argument 
+    C = ... or F = ... to specify a scale different from Kelvin."""
     if isinstance(value, str):
       if 'C' in value:   C = value.strip(' °C')
       elif 'F' in value: F = value.strip(' °F')
@@ -41,6 +46,9 @@ class temperature():
     else: self.K = value
 
 if __name__ == '__main__':
+  print("(Hit Ctrl-C to exit the loop.)")
   while True:
-    T = temperature( input("Please enter a temperature: "))
-    print("The temperature is %s." % T)
+    T = temperature(input("Please enter a temperature: "))
+    print(f"The temperature is {T}.")
+
+# eof
